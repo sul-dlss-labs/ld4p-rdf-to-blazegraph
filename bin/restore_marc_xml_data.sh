0e1318c581c3503b4bc5095d71031d55ba54d5cc
Naomi Dushay <ndushay@stanford.edu>
2017-02-23 19:10:31 -0800

    remove files irrelevant to the purpose of THIS repo

--- bin/restore_marc_xml_data.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Restoring missing MARC-XML from archives in ${LD4P_ARCHIVE_MARCXML}/"
-rsync -a --update ${LD4P_ARCHIVE_MARCXML}/ ${LD4P_MARCXML}/
-
--- bin/sge_cleanup_empty_outputs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-find ./ -type f -empty -name 'LD4P_TB_XML2RDF.o*' -exec rm {} \;
-find ./ -type f -empty -name 'LD4P_TB_XML2RDF.e*' -exec rm {} \;
-
