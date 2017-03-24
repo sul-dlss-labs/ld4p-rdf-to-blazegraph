0ff694aa02e2f3615822415754a03c78e8b88816
Darren L. Weber, Ph.D <dweber.consulting@gmail.com>
2017-03-23 22:58:01 -0700

    blazegraph_load_rdf.sh - replace all configs with input params

--- blazegraph_load_rdf.sh
+++ blazegraph_load_rdf.sh
@@ -1,45 +1,55 @@
 #!/bin/bash
 
-SCRIPT_PATH=$( cd $(dirname $0) && pwd -P )
-export LD4P_ROOT=$( cd "${SCRIPT_PATH}" && pwd -P )
-source ${LD4P_ROOT}/config/config.sh
-source ${LD4P_ROOT}/config/config_blazegraph.sh
-
-# ---
-# Check the configuration
-
-if [ ! -d "${LD4P_MARCRDF}" ]; then
-    echo "LD4P_MARCRDF path is not a directory: ${LD4P_MARCRDF}"
-    exit 1
-fi
-
-if [ -z "${BG_SPARQL}" ]; then
-    echo "BG_SPARQL is undefined."
-    exit 1
-fi
-
 # ---
 # Help
 
-if [ "$1" == '-h' -o "$1" == '--help' ]; then
+usage () {
     cat <<HERE
 Usage:
-$0 > log/blazegraph_load.log 2>&1
+$0 {blazegraph_sparql_uri} {rdf_data_directory } > log/blazegraph_load.log 2>&1
 
-NOTES:
-  - this script must be run on a system able to connect to a blazegraph SPARQL endpoint
-  - this script calls:
-    - ${LD4P_ROOT}/config/config.sh
-    - ${LD4P_ROOT}/config/config_blazegraph.sh
+Example:
+$0 http://localhost:9999/blazegraph/kb/sparql ./data/test
 
 HERE
+}
+
+if [ "$1" == '-h' -o "$1" == '--help' ]; then
+    usage
     exit
 fi
+
+
+# ---
+# Check the input parameters
+
+bg_sparql=$1
+
+if [ -z "${bg_sparql}" ]; then
+    echo
+    echo "ERROR: SPARQL URI is undefined."
+    echo
+    usage
+    exit 1
+fi
+
+rdf_path=$2
+
+if [ ! -d "${rdf_path}" ]; then
+    echo
+    echo "ERROR: RDF data path is not a directory: ${rdf_path}"
+    echo
+    usage
+    exit 1
+fi
+
 
 # ---
 # Load data
 
-echo "Blazegraph loading MARC-RDF files ${LD4P_MARCRDF}/*.rdf into graph: ${BG_GRAPH}"
-find ${LD4P_MARCRDF} -type f -name '*.rdf' \
-  -exec ${LD4P_ROOT}/bin/blazegraph_sparql_update.sh ${BG_SPARQL} {} \;
+SCRIPT_PATH=$( cd $(dirname $0) && pwd -P )
+
+echo "Loading RDF files ${rdf_path}/*.rdf into ${bg_sparql}"
+find ${rdf_path} -type f -name '*.rdf' \
+  -exec ${SCRIPT_PATH}/bin/blazegraph_sparql_update.sh ${bg_sparql} {} \;
 
